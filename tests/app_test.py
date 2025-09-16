7598b2daa7b5fecda5ac4ebe504fc34a94f027d7
sinval-jr <sinvallljunior@gmail.com>
2025-09-16 09:46:25 -0300

    Test with request - Google patents API request

--- tests/app_test.py
+++ tests/app_test.py
@@ -7,12 +7,16 @@ def fetch_page(url):
 
 def parse_html(html):
     soup = BeautifulSoup(html, 'html.parser')
-    num_patent = soup.find(id = 'pubnum', class_ = 'style-scope patent-result')
+    num_patent = soup.find(id = 'pubnum')
     title = soup.find('title').get_text()
-    print(f"Patent Number: {num_patent} \n Title: {title}")
+    resume = soup.find(id = 'p-0001')
+    print(f"Patent Number: {num_patent} \n Title: {title} \n Resume: {resume}")
+
+    patent_citation_quantity = soup.find(id = 'patentCitations')
+    print(f"Patent Citations: {patent_citation_quantity}")
 
 
 if __name__ == "__main__":
-    url = "https://patents.google.com/patent/US12367394B2/en?oq=US12367394B2"
+    url = "https://patents.google.com/patent/CN113010302B/en?q=(computer+quantum)&oq=computer+quantum"
     page_content = fetch_page(url)
     parse_html(page_content)
